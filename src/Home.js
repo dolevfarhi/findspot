1ddc583d3a16f721698e3ae0b8607bc3a53671c7
Alan Trope <atrope@gmail.com>
2018-07-10 19:04:43 +0300

    Adding Event on Click of Home+Map

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {GoogleApiWrapper} from 'google-maps-react';
 
+import TopHeader from "./TopHeader.js"
 import Map from "./Map.js"
 
 class Home extends Component {
@@ -8,22 +9,43 @@ class Home extends Component {
     super(props);
     this.state = {
       position: { lat: 32.0900011, lng: 34.8030246 },
-      markers: [{lat: 32.0850011, lng: 34.8030246},{lat: 32.0800011, lng: 34.8030246}],
+      markers: [],
+      isDone:false
     };
+    this.clickedSpot = this.clickedSpot.bind(this);
+
     this.findSpot = this.findSpot.bind(this);
-
   }
-  componentDidMount() { }
-
-  handleMarkerClick = () => {
-    console.log("CLICK");
-    this.setState({ isMarkerShown: !this.state.isMarkerShown })
-
-
+  componentDidMount() {
+    window.addEventListener('clickedSpot', this.clickedSpot);
   }
+
   componentWillUnmount(){
+    window.removeEventListener('clickedSpot', this.clickedSpot);
+  }
+
+  clickedSpot(e){
+    let position = e.position;
+    console.log(position);
+  }
+
+  componentWillMount(){
+    var self = this;
+    fetch('https://findspot.herokuapp.com/spot/search', {
+  method: 'POST',
+  headers: {"Content-Type": "application/json; charset=utf-8"},
+  body: JSON.stringify({lat:this.state.position.lat,long:this.state.position.lng,distance:2000})
+}).then(function(response) {
+  return response.json();
+}).then(function(data) {
+  console.log(data);
+  if (!data.length) alert("NO SPOTS CLOsE");
+  else data.forEach((el) => self.setState({markers:[...self.state.markers, {lat: el.location.coordinates[1], lng: el.location.coordinates[0]}]}));
+  self.setState({isDone:true});
+});
   }
   findSpot(){
+
   };
 
 
@@ -32,7 +54,8 @@ class Home extends Component {
   render() {
   return (
     <div>
-    <Map google={this.props.google} position={this.state.position} markers={this.state.markers}/>
+      <TopHeader money="8" points = "11"/>
+    { this.state.isDone && <Map google={this.props.google} position={this.state.position} markers={this.state.markers}/> }
     <button className="fixed-bottom findButton vw-100 text-center" onClick={this.findSpot}>FIND A SPOT <i className="ml-2 fas fa-car"></i></button>
     </div>
   )
--- src/Map.js
+++ src/Map.js
@@ -8,12 +8,20 @@ export class MapGoogle extends Component {
       position: props.position,
       markers: props.markers,
     };
+    this.onMarkerClick = this.onMarkerClick.bind(this);
   }
+  onMarkerClick(props, marker, e) {
+    var evt = document.createEvent("Events")
+    evt.initEvent("clickedSpot", true, true);
+    evt.position = props.position;
+    window.dispatchEvent(evt);
+  }
+
   render() {
     return (
       <Map google={this.props.google} style={{width: '100%', height: '100%', position: 'relative'}} className={'map'} zoom={15} initialCenter={this.state.position}>
-        <Marker title={'My Position.'} name={'Me'} position={this.state.position} />
-        {this.state.markers.map((marker,index) => (<Marker key={`marker${index}`} position={marker} />))}
+        <Marker title={'My Position.'} name={'Me'} position={this.state.position}  onClick={this.onMarkerClick}/>
+        {this.state.markers.map((marker,index) => (<Marker key={`marker${index}`} position={marker} onClick={this.onMarkerClick} />))}
       </Map>
     );
   }
